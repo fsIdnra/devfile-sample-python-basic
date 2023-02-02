789a6dc6e25624bf1a0fe3ada5651d900dd7c917
fsIdnra <124267663+fsIdnra@users.noreply.github.com>
2023-02-02 13:41:53 +0000

    count

--- app.py
+++ app.py
@@ -6,10 +6,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    r = redis.Redis(host=os.environ.get('REDIS_HOST'), port=os.environ.get('REDIS_PORT'), db=0)
-    count = r.get('count')
-    r.set('count', count+1)
-    return f"Hello World! Count: ${count}"
+    return f"Hello World!"
+
+@app.route('/count')
+def count():
+    try:
+        r = redis.Redis(host=os.environ.get('REDIS_HOST'), port=os.environ.get('REDIS_PORT'), db=0)
+        count = r.get('count')
+        r.set('count', count+1)
+        return f"Count: ${count}"
+    except Exception as e:
+        return(str(e))
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
